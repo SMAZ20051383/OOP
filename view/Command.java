33062dfeef29009a286030f359056dead6656b16
Amirali Fazeli <amiralifazeli291384@gmail.com>
2024-07-05 00:38:29 +0430

    second edition

--- view/Command.java
+++ view/Command.java
@@ -4,6 +4,233 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum Command {
+    number_1("  *  \n" +
+            "  *  \n" +
+            "  *  \n" +
+            "  *  \n" +
+            "*****\n"),
+    number_2("*****\n" +
+            "    *\n" +
+            "*****\n" +
+            "*    \n" +
+            "*****\n"),
+    number_3("*****\n" +
+            "    *\n" +
+            "*****\n" +
+            "    *\n" +
+            "*****\n"),
+    number_4("*   *\n" +
+            "*   *\n" +
+            "*****\n" +
+            "    *\n" +
+            "    *\n"),
+    number_5("*****\n" +
+            "*    \n" +
+            "*****\n" +
+            "    *\n" +
+            "*****\n"),
+    number_6("*****\n" +
+            "*    \n" +
+            "*****\n" +
+            "*   *\n" +
+            "*****\n"),
+    number_7("*****\n" +
+            "    *\n" +
+            "    *\n" +
+            "    *\n" +
+            "    *\n"),
+    number_8("*****\n" +
+            "*   *\n" +
+            "*****\n" +
+            "*   *\n" +
+            "*****\n"),
+    number_9("*****\n" +
+            "*   *\n" +
+            "*****\n" +
+            "    *\n" +
+            "*****\n"),
+    LETTER_A(
+            "  *  \n" +
+                    " * * \n" +
+                    "*****\n" +
+                    "*   *\n" +
+                    "*   *\n"
+    ),
+    LETTER_B(
+            "**** \n" +
+                    "*   *\n" +
+                    "**** \n" +
+                    "*   *\n" +
+                    "**** \n"
+    ),
+    LETTER_C(
+            "*****\n" +
+                    "*    \n" +
+                    "*    \n" +
+                    "*    \n" +
+                    "*****\n"
+    ),
+    LETTER_D(
+            "**** \n" +
+                    "*   *\n" +
+                    "*   *\n" +
+                    "*   *\n" +
+                    "**** \n"
+    ),
+    LETTER_E(
+            "*****\n" +
+                    "*    \n" +
+                    "*****\n" +
+                    "*    \n" +
+                    "*****\n"
+    ),
+    LETTER_F(
+            "*****\n" +
+                    "*    \n" +
+                    "*****\n" +
+                    "*    \n" +
+                    "*    \n"
+    ),
+    LETTER_G(
+            "*****\n" +
+                    "*    \n" +
+                    "* ***\n" +
+                    "*   *\n" +
+                    "*****\n"
+    ),
+    LETTER_H(
+            "*   *\n" +
+                    "*   *\n" +
+                    "*****\n" +
+                    "*   *\n" +
+                    "*   *\n"
+    ),
+    LETTER_I(
+            "*****\n" +
+                    "  *  \n" +
+                    "  *  \n" +
+                    "  *  \n" +
+                    "*****\n"
+    ),
+    LETTER_J(
+            "*****\n" +
+                    "   * \n" +
+                    "   * \n" +
+                    "*  * \n" +
+                    "**** \n"
+    ),
+    LETTER_K(
+            "*   *\n" +
+                    "*  * \n" +
+                    "***  \n" +
+                    "*  * \n" +
+                    "*   *\n"
+    ),
+    LETTER_L(
+            "*    \n" +
+                    "*    \n" +
+                    "*    \n" +
+                    "*    \n" +
+                    "*****\n"
+    ),
+    LETTER_M(
+            "*   *\n" +
+                    "** **\n" +
+                    "* * *\n" +
+                    "*   *\n" +
+                    "*   *\n"
+    ),
+    LETTER_N(
+            "*   *\n" +
+                    "**  *\n" +
+                    "* * *\n" +
+                    "*  **\n" +
+                    "*   *\n"
+    ),
+    LETTER_O(
+            "*****\n" +
+                    "*   *\n" +
+                    "*   *\n" +
+                    "*   *\n" +
+                    "*****\n"
+    ),
+    LETTER_P(
+            "*****\n" +
+                    "*   *\n" +
+                    "*****\n" +
+                    "*    \n" +
+                    "*    \n"
+    ),
+    LETTER_Q(
+            "*****\n" +
+                    "*   *\n" +
+                    "*   *\n" +
+                    "*  **\n" +
+                    "*****\n"
+    ),
+    LETTER_R(
+            "*****\n" +
+                    "*   *\n" +
+                    "*****\n" +
+                    "*  * \n" +
+                    "*   *\n"
+    ),
+    LETTER_S(
+            "*****\n" +
+                    "*    \n" +
+                    "*****\n" +
+                    "    *\n" +
+                    "*****\n"
+    ),
+    LETTER_T(
+            "*****\n" +
+                    "  *  \n" +
+                    "  *  \n" +
+                    "  *  \n" +
+                    "  *  \n"
+    ),
+    LETTER_U(
+            "*   *\n" +
+                    "*   *\n" +
+                    "*   *\n" +
+                    "*   *\n" +
+                    "*****\n"
+    ),
+    LETTER_V(
+            "*   *\n" +
+                    "*   *\n" +
+                    "*   *\n" +
+                    " * * \n" +
+                    "  *  \n"
+    ),
+    LETTER_W(
+            "*   *\n" +
+                    "*   *\n" +
+                    "* * *\n" +
+                    "** **\n" +
+                    "*   *\n"
+    ),
+    LETTER_X(
+            "*   *\n" +
+                    " * * \n" +
+                    "  *  \n" +
+                    " * * \n" +
+                    "*   *\n"
+    ),
+    LETTER_Y(
+            "*   *\n" +
+                    " * * \n" +
+                    "  *  \n" +
+                    "  *  \n" +
+                    "  *  \n"
+    ),
+    LETTER_Z(
+            "*****\n" +
+                    "   * \n" +
+                    "  *  \n" +
+                    " *   \n" +
+                    "*****\n"
+    ),
     SHOW_CURRENT_MENU("show current menu"),
     REGISTER("register username (?<username>.+) password (?<password>.*\\S)"),
     LOGIN("user login -u (?<username>.+) -p (?<password>.*\\S)"),
@@ -30,7 +257,10 @@ public enum Command {
     DEPLOY_TROOP("deploy troop (?<troopName>.+) in line (?<lineDirection>.+) and row (?<rowNumber>-?\\d+)"),
     DEPLOY_HEAL("deploy spell Heal in line (?<lineDirection>.+) and row (?<rowNumber>-?\\d+)"),
     DEPLOY_FIREBALL("deploy spell Fireball in line (?<lineDirection>.*\\S)"),
-    NEXT_TURN("next turn");
+    NEXT_TURN("next turn"),
+
+    ;
+
     private final String regex;
 
     Command(String regex) {
