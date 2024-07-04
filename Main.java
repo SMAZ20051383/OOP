bdb58ed15014ae7ac9562fbd9b4f70967b0a453e
Amirali Fazeli <amiralifazeli291384@gmail.com>
2024-07-04 23:36:13 +0430

    second edition

--- Main.java
+++ Main.java
@@ -1,7 +1,25 @@
 import controller.Controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:mydatabase.db";
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
     public static void main(String[] args) {
+        connect();
         new Controller().run();
     }
 }
